20a2d5c9b020f80576934e2cdd8de4f6504a845d
IndigoPurple <indigopurple@163.com>
2018-09-13 17:01:39 +0800

    Update createImg.m
    Improvement:Traversing all files in the folder

--- utils/createImg.m
+++ utils/createImg.m
@@ -1,21 +1,29 @@
 function createImg()
-
+%20180913-Improvement:Traversing all files in the folder
 clc;
 clear;
 
-video_path='/media/richard/eda30030-75b7-4aba-ab13-39e8846fd05a/home/richard/data/shanghai/mcam_14.mp4';
-video_obj=VideoReader(video_path);
+PathRoot = 'E:/primary_school_20180912_2/';
+list = dir(fullfile(PathRoot));
 
-frame_number=video_obj.NumberOfFrames;
+fileNum = length(list);
+for k = 3:fileNum %From 3 because list includes path . and ..
+    disp(list(k).name)
+    video_path=strcat('E:/primary_school_20180912_2/', list(k).name);
+    video_obj=VideoReader(video_path);
 
-for i=1:frame_number
-    image_name=strcat('/media/richard/eda30030-75b7-4aba-ab13-39e8846fd05a/home/richard/data/image_14/im_',num2str(i),'.jpg');
-    frame=read(video_obj,i);
-    if mod(i,10)==0
-        imwrite(frame,image_name,'jpg');
+    frame_number=video_obj.NumberOfFrames;
+
+    for i=1:frame_number
+        image_name=strcat('E:/data/img/primary_school_20180912_2/im_',num2str(i),'.jpg');
+        frame=read(video_obj,i);
+        if mod(i,10)==0
+            imwrite(frame,image_name,'jpg');
+        end
     end
 end
 
+
+
 disp('all images are written into directort image')
     
-
