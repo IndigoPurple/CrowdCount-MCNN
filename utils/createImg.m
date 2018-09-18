c7930059e5e11abdc0fc7ba66662778cd4752038
Richard <1272002241@qq.com>
2018-09-18 11:11:43 +0800

    数据标定代码更新

--- utils/createImg.m
+++ utils/createImg.m
@@ -6,22 +6,22 @@ clear;
 PathRoot = 'E:/primary_school_20180912_2/';
 list = dir(fullfile(PathRoot));
 
-fileNum = length(list);
-for k = 3:fileNum %From 3 because list includes path . and ..
-    disp(list(k).name)
-    video_path=strcat('E:/primary_school_20180912_2/', list(k).name);
-    video_obj=VideoReader(video_path);
+%fileNum = length(list);
+%for k = 3:fileNum %From 3 because list includes path . and ..
+ %   disp(list(k).name)
+  %  video_path=strcat('E:/primary_school_20180912_2/', list(k).name);
+    video_obj=VideoReader('F:\Giga\primary_school_20180912_1\cam_18312092.avi');
 
     frame_number=video_obj.NumberOfFrames;
 
     for i=1:frame_number
-        image_name=strcat('E:/data/img/primary_school_20180912_2/im_',num2str(k),num2str(i),'.jpg');
+        image_name=strcat('F:\Giga\primary_school_20180912_1_img\img_18312092\im_',num2str(i),'.jpg');
         frame=read(video_obj,i);
         if mod(i,10)==0
             imwrite(frame,image_name,'jpg');
         end
     end
-end
+%end
 
 
 
--- utils/getPositionInfo.m
+++ utils/getPositionInfo.m
@@ -1,21 +1,26 @@
 function getPositionInfo()
-
-im = imread('./GT_IMG/im_100.jpg');
-imshow(im);
+%图像标定函数，用来标定图像中的head所在位置
+%标定完成后之间点击关闭图像显示框
+clc;
+clear;
+im = imread('F:\Giga\primary_school_20180912_1_img\GT_IMG\im_50.jpg');
+g=imresize(im,0.25);
+imwrite(g,'F:\Giga2.0\CrowdCount-MCNN\data\original\shanghaitech\part_C_final\images\IMG_366.jpg')
+imshow(g);
 hold on;
 p=[];
 try
-    while true
-       a=ginput(1);
-       p=[p;a];
-       plot(a(1),a(2),'r*');
-    end    
+   while true
+     a=ginput(1);
+     p=[p;a];
+     plot(a(1),a(2),'r*');
+   end     
 catch  
-    b=size(p);
-    b=b(1);
-    image_info={};
-    image_info.location=p;
-    image_info.number=b;
-    save GT_INFO/GT_IMG_101.mat image_info;
+     b=size(p);
+     b=b(1);
+     image_info={};
+     image_info.location=p;
+     image_info.number=b;
+     save F:\Giga2.0\CrowdCount-MCNN\data\original\shanghaitech\part_C_final\ground_truth\GT_IMG_366.mat image_info;
 end   
 
